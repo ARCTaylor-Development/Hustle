18b340b8864b9724f879848043e51aca3350c141
ARCTaylor1231 <xfallenx1231@gmail.com>
2022-11-17 23:14:31 -0800

    Implemented basic shell for task buttons

---
+++ hustle-app/src/components/TaskBubble.vue
@@ -0,0 +1,76 @@
+<template>
+
+  <q-btn round :size="size">
+    <h1>{{ title }}</h1>
+  </q-btn>
+        
+  </template>
+
+  
+  
+  <script lang="ts">
+  import {
+    defineComponent,
+    PropType,
+    computed,
+    ref,
+    toRef,
+    Ref,
+  } from 'vue';
+
+  export default defineComponent({
+    name: 'TaskBubble',
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      size: {
+        type: String,
+        required: true
+      },
+      color: {
+        type: String,
+        required: true
+      }
+    },
+
+    setup (props) {
+      //Remove 'rem' and convert to int
+      function sizeToInt(str: string) {
+        return parseInt(str.substring(0, str.length-3))
+      }
+
+      const size = toRef(props, 'size')
+
+      const fontSize = computed(() => {
+        return ('' + sizeToInt(size.value) / 4.0 + 'rem');
+      });
+
+      return {
+        bubbleFontSize: fontSize
+      }
+
+    }
+
+  });
+
+
+  
+  </script>
+  
+  <style scoped>
+    .q-btn {
+      -ms-user-select: none;
+      user-select: none;
+
+      background-color: v-bind(color);
+    }
+
+    h1 {
+      color: white;
+      font-size: v-bind(bubbleFontSize);
+    }
+
+  </style>
+  
--- hustle-app/src/pages/IndexPage.vue
+++ hustle-app/src/pages/IndexPage.vue
@@ -1,49 +1,23 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+
+    <task-bubble title="Test Bubble" color="green" size="4rem"></task-bubble>
   </q-page>
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { defineComponent, ref } from 'vue';
+import TaskBubble from 'src/components/TaskBubble.vue';
+import { defineComponent} from 'vue';
 
 export default defineComponent({
   name: 'IndexPage',
-  components: { ExampleComponent },
-  setup () {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
-    return { todos, meta };
-  }
+  components: {TaskBubble }
 });
 </script>
+
+<style scoped>
+  .q-page {
+    /* background-color: black; */
+    background-image: url('..\assets\space-background.svg');
+  }
+</style>
